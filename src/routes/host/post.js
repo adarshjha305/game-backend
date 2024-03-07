f2b81af13b8efec0561a92ce9fdd073bf1185faf
Akshay Joshi <47622515+starDamage@users.noreply.github.com>
2024-03-07 15:12:58 +0530

    Update post.js

--- src/routes/host/post.js
+++ src/routes/host/post.js
@@ -211,7 +211,27 @@ export const loginHostHandler = async (req, res) => {
         )
       );
   } catch (error) {
-=======
+     if (error instanceof ValidationError || error instanceof CustomError) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .send(
+          responseGenerators({}, StatusCodes.BAD_REQUEST, error.message, 1)
+        );
+    }
+    console.log(JSON.stringify(error));
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(
+        responseGenerators(
+          {},
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          "Internal Server Error",
+          1
+        )
+      );
+  }
+};
+  }
 export const toggleBlockUnblockHandler = async (req, res) => {
   try {
     const { id } = req.params;
